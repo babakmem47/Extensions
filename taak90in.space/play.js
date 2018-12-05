783ca69c6fdc70ce71d8069810735611aba2393a
bk_Home <mohebbi@ebanksepah.ir>
2018-12-06 01:45:02 +0330

    97/9/15 1:44 am

--- taak90in.space/play.js
+++ taak90in.space/play.js
@@ -11,21 +11,15 @@ var waitForNewBurst = null;
 var historyOfBursts = [];
 var timeHistoryOfBursts = [];
 var newBurst = "-";
-var oldBurst = 666;
 var playing = false;
-var playingForFirstTime = false;
-var winning = true;
-var waiting = false;
-var initialAmount = 20;
 var index = 1.3;
-var wantedProfit = [7, 7, 5, 0, 0];
+var ExpectedProfit = [7, 7, 5, 0, 0];
 var newEntry = 0;
-var looseCount = 0;
-var looseCoverCount = 5;
+var loosesCount = 0;
 var split = ["", ""];
 var interest = 0;
 var betAmount = 0;
-var looseSum = 0;
+var loosesSum = 0;
 var greenLight = false;
 
 
@@ -40,36 +34,66 @@ function CheckForNewBurstEveryOneSecond() {
         newBurst = document.getElementsByClassName("crash-row")[0].children[0].innerHTML;
         console.log(newBurst);
         if (newBurst != "-") {   // New burst added
-            
+
             // stop waiting for new burst(for now!):
             clearInterval(waitForNewBurst);
-            
-            newEntry++
-            if (newEntry === 2) {  // counting new bursts until reach 2 to play for first time
-                playing = true;
-                playingForFirstTime = true;
+
+            newEntry++;
+            if (playing) {
+                if (newBurst < index) {   // loosing when playing: calculate loose count
+                    loosesCount++;
+                    loosesSum = betAmount + loosesSum;
+                    if (loosesCount >= 3) {
+                        playing = false;
+                    }
+                }
+                else if (newBurst >= index) {  // winning
+                    loosesCount = 0;
+                    loosesSum = 0;
+                }
             }
-            else if ((newBurst < index) && playing) {   // loosing when playing: calculate loose count
-                looseCount++;
-                looseSum = betAmount + looseSum;
+            else if (!playing) {             // if not play because of: 1.first time  2. loose 3th or 4th time           
+                if (loosesCount < 3) {
+                    if (newEntry >= 2) {  // counting new bursts until reach 2 to play for first time
+                        playing = true;
+                    }
+                }
+                else if (loosesCount >= 3) {
+                    if (newBurst >= 1.80) {
+                        greenLight = true;
+                        playing = true;
+                    }
+                }
             }
-            else if ((newBurst >= index) && playing) {  // winning
-                looseCount = 0;
-                looseSum = 0;
-            }
-            else if (newBurst >= 1.80 && !playing) {
-                greenLight = true;
-            }
-            
-            
-            
+
             // calculate remain interest:
             split = document.getElementsByClassName("top-link chips-amount")[0].innerHTML.split(" ")[0].split(",");
             interest = split[0].concat(split[1]);
 
+
             historyOfBursts.push(newBurst);
-            betAmount = Math.round(((wantedProfit[looseCount] + looseSum) - 0.4) / (index - 1));
-            console.log("new burst: ", newBurst, "   Entry: ", newEntry, "   looseCount: ", looseCount,"   betAmount: ", betAmount, " looseSum: ", looseSum);
+
+            ////// increase profit (if possible) depend on interest grow ///////////////////////
+            console.log(ExpectedProfit);
+            ExpectedProfit[0]++;
+            console.log(ExpectedProfit);
+            var sum = 0;
+            for (var i = 0; i < ExpectedProfit.length; i++) {
+                sum += Math.round(((ExpectedProfit[i] + sum) - 0.4) / (index - 1));
+            }
+            console.log("sum: ", sum);
+            if (sum < interest) {
+                // successfully increase ExpectedProfit!!  because interest is enough
+            }
+            else {
+                ExpectedProfit[0]--;    // interest is not enough. So rollback to previous value;
+            }
+            ////////////////////////////////////////////////////////////////////////////////////
+
+            console.log(ExpectedProfit);
+            betAmount = Math.round(((ExpectedProfit[loosesCount] + loosesSum) - 0.4) / (index - 1));
+            console.log("looseCount: ", loosesCount, "  looseSum: ", loosesSum, "   betAmount: ", betAmount);
+            console.log("playing: ", playing, "  greenLight: ", greenLight, "  interest: ", interest);
 
             // wait 4 second and act:
             setTimeout(WaitForFourSeconds, 4000);
@@ -87,28 +111,13 @@ function CheckForNewBurstEveryOneSecond() {
 function WaitForFourSeconds() {
     // After 4 Second passed:
     // Initialize variables:
-    if (playing) {        
-        
-        if (playingForFirstTime) {    // start playing for first time:
-            document.getElementsByClassName("game-amount")[0].value = betAmount;
-            playingForFirstTime = false;
-        }
-        else if (looseCount === 0) {  // winning 
-            document.getElementsByClassName("game-amount")[0].value = betAmount;
-        }
-        else if (looseCount === 1) {  // loose for first time
-            document.getElementsByClassName("game-amount")[0].value = betAmount;
-        }
-        else if (looseCount === 2) {  // loose for second time
-            document.getElementsByClassName("game-amount")[0].value = betAmount;
-        }
-        else if (looseCount === 3) {  // loose for third time
-
-            playing = false;
-        }
+    if (playing) {
+        document.getElementsByClassName("cashout-amount")[0].value = index;
+        document.getElementsByClassName("game-amount")[0].value = betAmount;
+        var betButton = document.getElementsByClassName("place-bet")[0];
+        betButton.click();
     }
-    document.getElementsByClassName("cashout-amount")[0].value = index;
-    
+
     // activate waiting for new burst again:
     waitForNewBurst = setInterval(CheckForNewBurstEveryOneSecond, 1000);
 }
