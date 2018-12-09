cf3d92640e903332ab824ca02646be53c97f5447
babak35102 <babak35102@gmail.com>
2018-12-09 16:00:21 +0330

    97/9/18 16:00

--- 1.3 Ver.2/play.js
+++ 1.3 Ver.2/play.js
@@ -3,21 +3,35 @@
 // 1. Start play after 2 new burst with index: 1.30 ExpectedProfit = [6, 7, 0, 0] interest: 3731.  
 // 2. After first loose play with index: 1.3 again. 
 //    After 2nd loose play with index:   1.2. 
-//    After 3rd loose, stop playing for 10 games 
-// 3. Wait until see one of below situation: 
+//    After 3rd loose, stop playing. 
+// 3. Wait until see one of below situation(wait for 1.2): 
 //                                          Not after: a series of red  
 //                                          Not after: red red green green   
 //                                          Not after: green 1.2 green
 //                                          Not after: big fat green
-//                                          0. green 1.03  play!
+//                                          Not after: green green 1.46 green 1.76 
+//                                          Not after: green green green 1.37  
+//                                          Not after: 4 consecutive green play
+//                                          Not after: 5 consecutive green play
+//                                          یکی در میون آخری زیر 1.40 برو تو
+//                                          0. green green green green 1.09  play!
+//                                          0. green green green green 1.00  play!
+//                                          0. green green green 1.35 green 0.00 play!
+//                                          0. green green green green 1.09 green 1.56 play!
 //                                          0. green 1.03 green 1.21 play  
+//                                          0. green green 1.58 green 1.00 play  
+//                                          0. green green 1.22 green green 1.05 play  
+//                                          0. green green 1.11 1.20 1.06 green 1.31 play  
+//                                          0. 1.25 1.14 1.70 1.13 green green 1.12 play  
+//                                          0. 1.35 green 1.06 1.22 green 1.04 play  
+//                                          0. 1.12 green green 0.00 green 1.19 1.01 play  
 //                                          1. a less than 1.2 between 2 green number(last number not > 5).
 //                                          1. a less than 1.2 after 5 out of 6 green.
 //                                          2. green green 1.19 green 1.07 play
+//                                          2. 1.04 1.61 green green green 1.07 play
 //                                          3. green green green green 1.15 play
 //                                          3. green green 1.22 green green play
 //                                          4. green 1.27 1.12 1.02 green play
-//                                          4. 5 consecutive >=1.8
 //                                          5. a series of 2 or more less than 1.2 when finished(series between greens).
 //                                          5. green 1.11 1.00 1.02 
 //                                          6. 1.00 green green green 1.14  play
@@ -44,7 +58,7 @@ var split = ["", ""];
 var interest = 3731;
 var betAmount = 0;
 var loosesSum = 0;
-var greenLight = false;
+var playingForFirstTime = true;
 
 
 window.onload = function () {
@@ -77,14 +91,14 @@ function CheckForNewBurstEveryOneSecond() {
                 }
             }
             else if (!playing) {             // if not play because of: 1.first time  2. loose 3th or 4th time           
-                if (loosesCount < 3) {
+                if (playingForFirstTime) {
                     if (newEntry >= 2) {  // counting new bursts until reach 2 to play for first time
                         playing = true;
+                        playingForFirstTime = false;
                     }
                 }
                 else if (loosesCount >= 3) {
                     if (newBurst >= 1.80) {
-                        greenLight = true;
                         playing = true;
                     }
                 }
@@ -117,7 +131,7 @@ function CheckForNewBurstEveryOneSecond() {
             console.log(ExpectedProfit);
             betAmount = Math.round(((ExpectedProfit[loosesCount] + loosesSum) - 0.4) / (index - 1));
             console.log("looseCount: ", loosesCount, "  looseSum: ", loosesSum, "   betAmount: ", betAmount);
-            console.log("playing: ", playing, "  greenLight: ", greenLight, "  interest: ", interest);
+            console.log("playing: ", playing, "  greenLight: ", playingForFirstTime, "  interest: ", interest);
 
             // wait 4 second and act:
             setTimeout(WaitForFourSeconds, 4000);
